fcaacbf40f6cdd8c71c3d19a87054bbfbc778e8e
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-03-12 16:02:03 +0100

    Added ad-hoc lesson for serialization

---
+++ LearnSerialization/Models/Person.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LearnSerialization;
+
+public class Person
+{
+  public string Name { get; set; }
+  [JsonPropertyName("lastname")]
+  public string Surname { get; set; }
+  public int Age { get; set; }
+  public Address Address { get; set; }
+
+  public void SerializeToFile(string path)
+  {
+    var json = JsonSerializer.Serialize<Person>(
+      this,
+      new JsonSerializerOptions()
+      {
+        WriteIndented = true,
+        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+      }
+    );
+    // check if file at path is already existing
+    if (!File.Exists(path))
+      File.Create(path).Close();
+    File.WriteAllText(path, json);
+  }
+  public static Person DeserializeFromFile(string path)
+  {
+    if (!File.Exists(path))
+      throw new FileNotFoundException("File not found", path);
+    var json = File.ReadAllText(path);
+    return JsonSerializer.Deserialize<Person>(json);
+  }
+
+  public override string ToString()
+  {
+    return $"""
+        Name: {Name}
+        Surname: {Surname}
+        Age: {Age}
+        Address: {Address.Street} {Address.Number}
+        """;
+  }
+}
+
+public class Address
+{
+  public string Street { get; set; }
+  public int Number { get; set; }
+}
---
+++ LearnSerialization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using LearnSerialization;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+var p1 = new Person()
+{
+  Name = "Tobi",
+  Age = 25,
+  Surname = "Wehrle",
+  Address = new Address() { Street = "Neckargartacherstraße", Number = 420 }
+};
+var p2 = new Person()
+{
+  Name = "Alfred",
+  Age = 31,
+  Surname = "Müller",
+  Address = new Address() { Street = "Buxingerstraße", Number = 42 }
+};
+
+p1.SerializeToFile("./person1.json");
+
+
+var deserializedPerson = Person.DeserializeFromFile("./person1.json");
+
+Console.WriteLine(deserializedPerson);
+
+Console.WriteLine("---YAML---");
+
+
+var yamlSerializer = new SerializerBuilder()
+  .WithNamingConvention(CamelCaseNamingConvention.Instance)
+  .Build();
+
+var yaml = yamlSerializer.Serialize(p1);
+
+Console.WriteLine("Person 1 as Serialized:");
+Console.WriteLine(yaml);
+
+File.WriteAllText("./person.yaml", yaml);
+
+var dYaml = File.ReadAllText("./person.yaml");
+
+var yamlDeserializer = new DeserializerBuilder()
+  .WithNamingConvention(CamelCaseNamingConvention.Instance)
+  .Build();
+
+var yamlPerson = yamlDeserializer.Deserialize<Person>(dYaml);
+
+Console.WriteLine("Person 1 as Deserialized:");
+
+Console.WriteLine(yamlPerson);
